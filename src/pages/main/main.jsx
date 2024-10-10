466e0bf0a807d2a2a81fd17fb5ebb27b91ec26fa
awwwdde <sjuu1337@gmail.com>
2024-10-11 02:33:21 +0300

    Added text to main page

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import './Main.scss'
 function main() {
   return (
-    <div>
-      hi
+    <div className="main">
+        <div className="main-text">
+            <div className="main-text_first">
+                <div className="main-text_first__title">Welcome to my portfolio site.</div>
+                <div className="main-text_first__subtitle">I am very happy to see you here!</div>
+            </div>
+            <div className="main-text_second">
+                <div className="main-text_second__title">Please select the answer option you are interested in <span className="blink">_</span></div>
+            </div>
+        </div>
     </div>
   )
 }
