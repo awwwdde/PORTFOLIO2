7fa0b3a1f91993d53ab2c9111eb7e59e6d101b05
awwwdde <sjuu1337@gmail.com>
2024-10-12 03:00:06 +0300

    Something new

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Start from './pages/start/start.jsx'
 import Portfolio from './pages/main/main.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './graphics/styles/style.scss'
+import About from './pages/about/About.jsx';
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
        <Route path="/" element={<Preloader />} />
         <Route path="/start" element={<Start />} />
         <Route path="/portfolio" element={<Portfolio/>}/>
+        <Route path="/portfolio/me" element={<About/>}/>
       </Routes>
     </BrowserRouter>
     </div>
---
+++ src/pages/about/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './about.scss';
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <div className="bento-box">
+        <div className="box box1">Блок 1</div>
+        <div className="box box2">Блок 2</div>
+        <div className="box box3">Блок 3</div>
+        <div className="box box4">Блок 4</div>
+        <div className="box box5">Блок 5</div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
