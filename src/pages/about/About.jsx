8a1124a9b56a99d1120bdaaea67ea7f1b38b884f
awwwdde <sjuu1337@gmail.com>
2024-10-13 02:46:28 +0300

    Update

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import './about.scss';
-
 const About = () => {
   return (
     <div className="about">
-    <div className="about-container">
-      <div className="about-container__box box1"></div>
-      <div className="about-container__box box2"></div>
-      <div className="about-container__box box3"></div>
-      <div className="about-container__box box4"></div>
-      <div className="about-container__box box5"></div>
-      <div className="about-container__box box6"></div>
+      <div className="about-container">
+        <div className="about-container__box box1"></div>
+        <div className="about-container__box box2">AWWWDDE</div>
+        <div className="about-container__box box3">Skills</div>
+        <div className="about-container__box box4">About</div>
+        <div className="about-container__box box5">gif</div>
+        <div className="about-container__box box6">Links</div>
+        <div className="about-container__box box7">Education</div>
+        <div className="about-container__box box8">Portfolio</div>
+        <div className="about-container__box box9">Contact</div>
+      </div>
     </div>
-    </div>
-
   );
 };
 
