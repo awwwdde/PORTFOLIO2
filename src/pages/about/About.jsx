de75ed5036575c2897602bd0b148c2bf2e1843ab
awwwdde <sjuu1337@gmail.com>
2024-10-12 23:20:22 +0300

    New

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -3,15 +3,17 @@ import './about.scss';
 
 const About = () => {
   return (
+    <div className="about">
     <div className="about-container">
-      <div className="bento-box">
-        <div className="box box1">Блок 1</div>
-        <div className="box box2">Блок 2</div>
-        <div className="box box3">Блок 3</div>
-        <div className="box box4">Блок 4</div>
-        <div className="box box5">Блок 5</div>
-      </div>
+      <div className="about-container__box box1"></div>
+      <div className="about-container__box box2"></div>
+      <div className="about-container__box box3"></div>
+      <div className="about-container__box box4"></div>
+      <div className="about-container__box box5"></div>
+      <div className="about-container__box box6"></div>
     </div>
+    </div>
+
   );
 };
 
