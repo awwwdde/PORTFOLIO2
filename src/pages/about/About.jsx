f3bfdd3728d26988fb5664b736c901881eb4da21
awwwdde <sjuu1337@gmail.com>
2024-10-17 01:59:48 +0300

    Update page about

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './about.scss';
-import logo from '../../graphics/images/logo.svg'
+import logo from '../../graphics/images/logo.svg'; // Импортируем логотип
 import SphereBackground from '../../components/bg/SphereBackground';
+
 const About = () => {
+  // Массивы с текстом и названиями меню
+  const texts = [
+    "Pellentesque iaculis eleifend nisi, at ullamcorper ante ultrices eu. Aliquam felis nibh, lacinia laoreet leo pharetra, gravida dapibus ligula. Nulla eu est id lectus pharetra congue et et magna. Donec hendrerit tellus ac nibh pellentesque maximus. Sed lobortis sodales orci nec maximus. Quisque non quam tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus facilisis libero elit, non ultricies nulla molestie et. Nunc nibh ipsum, pretium et molestie pharetra, rutrum quis nisi. Cras ornare dictum nibh non imperdiet. Proin lobortis mi nec nibh iaculis iaculis. Vestibulum at congue mi. Sed ut tellus convallis, condimentum leo ultrices, tristique nibh. Suspendisse sed dui lacus.",
+    "Это второй текст.",
+    "Это третий текст."
+  ];
+
+  const menuItems = [
+    "ABOUT",
+    "PORTFOLIO",
+    "CONTACT"
+  ];
+
+  // Состояние для текущего текста
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
+
+  // Функция для изменения текста
+  const handleMenuClick = (index) => {
+    setCurrentTextIndex(index);
+  };
+
   return (
     <div className="about">
-      
       <div className="about-container">
-        <SphereBackground/>
-        <div className="about-container__box box1">
-          <img src={logo} alt="" className="about-container__box-img" />
+        <SphereBackground />
+        <div className="about-container__block">
+          {texts[currentTextIndex]} {/* Отображаем текущий текст */}
         </div>
-        <div className="about-container__box box2">AWWWDDE</div>
-        <div className="about-container__box box3">Skills</div>
-        <div className="about-container__box box4">About</div>
-        <div className="about-container__box box5">gif</div>
-        <div className="about-container__box box6">Links</div>
-        <div className="about-container__box box7">Education</div>
-        <div className="about-container__box box8">Portfolio</div>
-        <div className="about-container__box box9">Contact</div>
+      </div>
+      <div className="about-menu">
+        <img src={logo} alt="Логотип" className="about-menu__logo" /> {/* Логотип */}
+        {menuItems.map((item, index) => (
+          <div 
+            key={index} 
+            className="about-menu__item" 
+            onClick={() => handleMenuClick(index)}
+          >
+            {item}
+          </div>
+        ))}
       </div>
     </div>
   );
