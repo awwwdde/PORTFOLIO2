dd356c25ec8ee459f3c1315cdc9fbc2a5ff7ce8e
awwwdde <sjuu1337@gmail.com>
2024-10-20 02:00:40 +0300

    contact block update

--- src/components/contact block/cblock.jsx
+++ src/components/contact block/cblock.jsx
@@ -9,13 +9,16 @@ const Cblock = () => {
                 If you would like to get in touch, feel free to reach out via email or connect with me on social media!
             </p>
             <p className={styles.text}>
-                Email: aufjakegutlan@gmail.com
+                aufjakegutlan@gmail.com
             </p>
             <p className={styles.text}>
-                click: <a href="https://t.me/adolfvav" target="_blank" rel="noopener noreferrer" className={styles.link}>Telegram</a>
+            <a href="https://t.me/adolfvav" target="_blank" rel="noopener noreferrer" className={styles.link}>Telegram</a>
             </p>
             <p className={styles.text}>
-                click: <a href="https://github.com/awwwdde" target="_blank" rel="noopener noreferrer" className={styles.link}>Github</a>
+            <a href="https://github.com/awwwdde" target="_blank" rel="noopener noreferrer" className={styles.link}>Github</a>
+            </p>
+            <p className={styles.text}>
+            <a href="https://dribbble.com/awwwdde" target="_blank" rel="noopener noreferrer" className={styles.link}>Dribbble</a>
             </p>
         </div>
     );
